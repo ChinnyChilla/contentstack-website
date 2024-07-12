9bb941bd19fbe177271fbc392165e124f867cff9
ChinnyChilla <michaelzheng04@gmail.com>
2024-07-12 13:58:00 -0400

    got leaderboard to show up

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,52 +1,137 @@
-import { FunctionComponent } from "react";
+import React from "react";
 import "./Leaderboard.css";
+import LeaderboardCard from "./LeaderboardCard";
 
-export type LeaderboardType = {
-  className?: string;
+export interface LeaderboardEntryType {
+	name: string,
+	score: number,
+	image: string
+}
+interface LeaderboardType {
+  topic: string;
+  highest: Array<LeaderboardEntryType>
 };
 
-const Leaderboard: FunctionComponent<LeaderboardType> = ({
-  className = "",
-}) => {
-  return (
-    <div className={`leaderboard1 ${className}`}>
-      <div className="leaderboard-child" />
-      <div className="leaderboard-item" />
-      <div className="leaderboard-inner" />
-      <div className="rectangle-div" />
-      <b className="monthly-leaderboard">Monthly Leaderboard</b>
-      <div className="leaderboard-minimalistic">
-        <div className="leaderboard-box-small">
-          <div className="avatar-21">
-            <img className="frame-icon17" alt="" src="/frame-6.svg" />
-            <div className="frame17">
-              <div className="text17" />
-            </div>
-          </div>
-          <div className="name-4">Name 2</div>
-          <div className="avatar-222">
-            <img className="frame-icon17" alt="" src="/frame-6.svg" />
-            <div className="frame18">
-              <div className="text18" />
-            </div>
-          </div>
-          <div className="name-5">Name 3</div>
-          <div className="avatar-2310">
-            <img className="frame-icon17" alt="" src="/frame-6.svg" />
-            <div className="frame18">
-              <div className="text18" />
-            </div>
-          </div>
-          <b className="top-recievers">{`Top <topic>`}</b>
-          <div className="name-6">Name 1</div>
-          <img className="menu-5-4" alt="" src="/menu-5-4.svg" />
-          <img className="menu-5-5" alt="" src="/menu-5-5.svg" />
-          <img className="menu-5-6" alt="" src="/menu-5-6.svg" />
-          <img className="trophy-2-icon" alt="" src="/trophy-2.svg" />
-        </div>
-      </div>
-    </div>
-  );
-};
+enum TopicChoices {
+	"Top Booster" = "boost_given",
+	"Top Boosted" = "boost_recieved"
+}
+interface UserType {
+	title: string,
+	display_name: string,
+	boost_given: number,
+	boost_recieved: number,
+	username: string
+	profile_picture: {
+		href: string;
+		title: string;
+	};
+}
+interface LeaderboardState {
+	users: Array<UserType>,
+	leaderboard: Array<LeaderboardType>
+	isLoading: boolean
+}
 
+class Leaderboard extends React.Component<{}, LeaderboardState> {
+	constructor(props: any) {
+		super(props);
+
+		this.state = {
+			users: [],
+			isLoading: true,
+			leaderboard: []
+		}
+	}
+
+	getHighest(amount: number, users: Array<UserType>) {
+		var leaderboard = []
+		for (const [name,choice] of Object.entries(TopicChoices)) {
+			const usersSorted = [...users].sort((u1, u2) => u2[choice] - u1[choice])
+
+			const topUsers = usersSorted.slice(0, amount);
+			var convertedArray: Array<LeaderboardEntryType> = []
+			for (var i=0; i<amount; i++) {
+				const user = topUsers[i];
+				convertedArray.push({
+					name: user.display_name,
+					score: user[choice],
+					image: user.profile_picture.href
+				})
+			}
+			leaderboard.push({
+				topic: name as keyof typeof TopicChoices,
+				highest: convertedArray
+		})
+		}
+		this.setState({
+			isLoading: false,
+			leaderboard: leaderboard
+		})
+	}
+
+	componentDidMount(): void {
+		const parameters = new URLSearchParams({
+			environment: process.env.REACT_APP_CONTENTSTACK_ENVIRONMENT_NAME || "",
+			locale: "en-us",
+			include_fallback: "true",
+			include_branch: "false"
+		});
+		const headers = new Headers();
+		headers.set("api_key", process.env.REACT_APP_CONTENTSTACK_API_KEY || "");
+		headers.set("access_token", process.env.REACT_APP_CONTENTSTACK_ACCESS_TOKEN || "");
+		const url = `${process.env.REACT_APP_CONTENTSTACK_BASE_URL}/v3/content_types/slack_user/entries?${parameters.toString()}`;
+		console.log(`Fetched url ${url}`);
+		fetch(url, {
+			method: "GET",
+			headers: headers
+		}).then(response => {
+			if (!response.ok) {
+				console.error('HTTP error:', response.status, response.statusText);
+				throw new Error('Network response was not ok');
+			}
+			return response.json();
+		}).then(data => {
+			console.log(data.entries)
+			this.getHighest(3, data.entries)
+		}).catch(error => {
+			console.error('Fetch error:', error);
+		});
+
+	}
+
+	render() {
+		if (this.state.isLoading) {
+			return (
+				<div className="leaderboard-container">
+					<div className="leaderboard-child" />
+					
+					<div className="monthly-leaderboard">
+						<span>
+							Loading...
+						</span>
+					</div>
+				
+					
+				</div>
+			);
+		}
+		return (
+			<div className="leaderboard-container">
+				<div className="leaderboard-child" />
+				<b className="monthly-leaderboard">Monthly Leaderboard</b>
+				{this.state.leaderboard.map((leaderboardRanking, index) => {
+					return (
+						<LeaderboardCard
+							topic={leaderboardRanking.topic}
+							leaderboard={leaderboardRanking.highest}
+							key={index}
+						/>
+					);
+				})}
+			</div >
+		);
+	}
+	
+}
 export default Leaderboard;
---
+++ src/components/LeaderboardCard.tsx
@@ -0,0 +1,28 @@
+import react from "react"
+import LeaderboardEntry from "./LeaderboardEntry"
+import "./LeaderboardCard.css"
+import { LeaderboardEntryType } from "./Leaderboard"
+
+
+const LeaderboardCard = ({topic, leaderboard}: {topic: string, leaderboard: Array<LeaderboardEntryType>}) => {
+
+	return (
+		<div className="leaderboard-card-container">
+			<div className="leaderboard-card-topic">
+				<span>
+				{topic}
+				</span>
+			</div>
+
+			<div className="leaderboard-entries-container">
+				{leaderboard.map((entry, index) => {
+					return (
+						<LeaderboardEntry score={entry.score} profile_url={entry.image} display_name={entry.name} key={index} />
+					)
+				})}
+			</div>
+		</div>
+	)
+}
+
+export default LeaderboardCard
